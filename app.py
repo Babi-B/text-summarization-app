948a4541231253bf1712a5ce818128877af9b7ff
Babi-B <babibyoolah@gmail.com>
2024-06-22 14:46:42 +0100

    initial commit -m

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+
+
+if __name__ == "__main__":
+    main()
